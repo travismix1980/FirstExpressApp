0c2203e5edbf09d510f1b90d211f86b363c54475
Travis Mix <travismix1980@gmail.com>
2020-10-02 18:34:26 -0700

    This is turning into an e-shop so ending this and transfering further code work to a new repository

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const path = require('path');
 const app = express();
 
 const adminRoutes = require('./routes/admin');
@@ -9,7 +10,7 @@ app.use(bodyParser.urlencoded({extended: false}));
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 app.use((req, res) => {
-    res.status(404).send('<h1>404 Page not found</h1>');
+    res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
 });
 
 app.listen(3000);
